955324711ecf73bc52ad856521a9ca16559d8321
David Gauntlett <david.m.gauntlett@gmail.com>
2025-01-16 19:45:42 -0500

    better footer and nav

--- app/footer.tsx
+++ app/footer.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div className={"h-24 mt-auto"}>foot</div>
+      <div className="flex border-t-2 border-t-slate-400 max-h-[50px] mt-auto h-10 p-3">
+        <div className={"w-20"}></div>
+        <div className="mx-auto">
+          Broken Gaming League
+        </div>
+        <div className="text-right w-20">
+          <Link href={"mailto:"}>Contact Us</Link>
+        </div>
+      </div>
   )
 }
 
--- app/navbar.tsx
+++ app/navbar.tsx
@@ -2,15 +2,25 @@ import React from 'react';
 import Image from "next/image";
 import Link from "next/link";
 import {SignedIn, SignedOut, SignInButton, UserButton} from "@clerk/nextjs";
+import {PrismaClient} from "@prisma/client";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const prisma = new PrismaClient()
+  const leagues = await prisma.league.findMany({
+    select: {
+      id: true,
+      acronymn: true,
+    }
+  })
   return (
     <div className={"flex justify-between px-10 py-3 items-center"}>
       <Link href={"/"}>
         <Image src={"/assets/v2 Vector.svg"} alt={"Broken Gaming League Logo"} width={100} height={100}/>
       </Link>
-      <div>
-        <Link href={"/bwrl"} className={"hover:border-slate-700 border-b-2"}>BWRL</Link>
+      <div className={"flex gap-5"}>
+        {leagues.map(league => (
+          <Link href={`/league/${league.id}`} className={"hover:border-slate-700 border-b-2"} key={league.id}>{league.acronymn}</Link>
+        ))}
       </div>
       <div>
         <SignedOut>
