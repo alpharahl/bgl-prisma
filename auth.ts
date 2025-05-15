745be0e209e2bcf8f57f91d8d6d83e2c6d0786e9
David Gauntlett <david.m.gauntlett@gmail.com>
2025-05-14 23:12:45 -0400

    specify secret as next_private

--- auth.ts
+++ auth.ts
@@ -3,6 +3,11 @@ import Discord from "@auth/core/providers/discord";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
-    Discord
+    Discord(
+      {
+        clientId: process.env.NEXT_PRIVATE_AUTH_DISCORD_ID,
+        clientSecret: process.env.NEXT_PRIVATE_AUTH_DISCORD_SECRET,
+      })
   ],
+  secret: process.env.NEXT_PRIVATE_AUTH_SECRET,
 })
