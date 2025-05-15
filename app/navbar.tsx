853550a3634f8ecdac3a739067f0a4b3da7095e3
David Gauntlett <david.m.gauntlett@gmail.com>
2025-05-14 23:50:50 -0400

    was setting logs in the wrong place

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -6,11 +6,6 @@ import {auth} from "@/auth";
 
 const Navbar = async () => {
   const session = await auth()
-  let falseSession = false;
-  // @ts-ignore
-  if (session?.message === "There was a problem with the server configuration. Check the server logs for more information."){
-    falseSession = true;
-  }
   return (
 
       <div className={"flex justify-between px-10 py-3 items-center"}>
@@ -18,9 +13,7 @@ const Navbar = async () => {
           <Image src={"/assets/v2 Vector.svg"} alt={"Broken Gaming League Logo"} width={100} height={100}/>
         </Link>
         <div>
-          <div>Nav auth</div>
-          {JSON.stringify(session)}
-          {(session && !falseSession) ? session.user?.name : <SignInWithDiscord/>}
+          {session ? session.user?.name : <SignInWithDiscord/>}
         </div>
       </div>
   )
