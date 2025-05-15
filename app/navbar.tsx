83deb0c480b5d3bd7dcf5da68753479bf4a3ff09
David Gauntlett <david.m.gauntlett@gmail.com>
2025-05-14 23:04:46 -0400

    logging session

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -6,8 +6,7 @@ import {auth} from "@/auth";
 
 const Navbar = async () => {
   const session = await auth()
-
-
+  console.log('session', session)
   return (
 
       <div className={"flex justify-between px-10 py-3 items-center"}>
@@ -15,6 +14,7 @@ const Navbar = async () => {
           <Image src={"/assets/v2 Vector.svg"} alt={"Broken Gaming League Logo"} width={100} height={100}/>
         </Link>
         <div>
+          <div>Nav auth</div>
           {JSON.stringify(session)}
           {session ? session.user?.name : <SignInWithDiscord/>}
         </div>
