929d955e8c2b2415fbe35212bc87569e1c857fe2
David Gauntlett <david.m.gauntlett@gmail.com>
2025-05-14 21:24:56 -0400

    fix clerk leftover

--- utils/admin.ts
+++ utils/admin.ts
@@ -1,14 +1,13 @@
-import {currentUser} from "@clerk/nextjs/server";
-import {PrismaClient} from "@prisma/client";
+import { auth } from "@/auth";
 import prisma from "@/lib/prisma";
 
 
 export const isAdmin = async () => {
-  const user = await currentUser();
-  if (!user){return false}
+  const session = await auth();
+  if (!session || !session.user){return false}
   const admin = await prisma.admins.findUnique({
     where: {
-      id: user.id,
+      id: session.user.email,
     }
   })
   return !!admin;
