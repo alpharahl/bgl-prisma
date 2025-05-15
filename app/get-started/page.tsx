ce1197bb240d0e6aa3c8bda7baa35e89fe7c4dd5
David Gauntlett <david.m.gauntlett@gmail.com>
2025-05-15 00:46:17 -0400

    fixing style in get-started

--- app/get-started/page.tsx
+++ app/get-started/page.tsx
@@ -7,13 +7,13 @@ import Cars from '@/assets/bwrl-cars.avif'
 const Page = () => {
   return (
     <div>
-      <div className={"flex flex-col gap-8 row-start-2 w-full max-w-4xl mx-auto"}>
+      <div className={"flex flex-col gap-8 row-start-2 w-full max-w-4xl mx-auto px-5"}>
         <h1 className={"text-3xl text-primary"}>Getting Started</h1>
         <p>
           Thanks for showing interest in BWRL. Joining up is easy.
         </p>
         <h2 className={"text-primary text-2xl"}>Discord</h2>
-        <p className={"text-lg items-center flex gap-5"}>First join us on
+        <p className={"text-lg items-center flex gap-5 flex-wrap"}>First join us on
           <Link href={"https://discord.gg/9kDGGVPQ"}
                 className={""}>
             <Image src={Discord.src}
