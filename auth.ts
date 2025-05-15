293de7c6b1e27cf875c48792ff6dbb340cad1786
David Gauntlett <david.m.gauntlett@gmail.com>
2025-05-14 23:36:18 -0400

    changing package-lock, removing next_private

--- auth.ts
+++ auth.ts
@@ -3,11 +3,6 @@ import Discord from "@auth/core/providers/discord";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
-    Discord(
-      {
-        clientId: process.env.NEXT_PRIVATE_AUTH_DISCORD_ID,
-        clientSecret: process.env.NEXT_PRIVATE_AUTH_DISCORD_SECRET,
-      })
-  ],
-  secret: process.env.NEXT_PRIVATE_AUTH_SECRET,
+    Discord
+  ]
 })
