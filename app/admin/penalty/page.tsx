3ad178e532da03740d2adc00200666f83e95965f
David Gauntlett <david.m.gauntlett@gmail.com>
2025-06-13 21:09:58 -0400

    remove ReportTester component and its import from penalty page

--- app/admin/penalty/page.tsx
+++ app/admin/penalty/page.tsx
@@ -5,7 +5,6 @@ import PenaltyCreateInput = Prisma.PenaltyCreateInput;
 import isAdmin from "@/lib/isAdmin";
 import AddPenalty from "@/app/admin/penalty/addPenalty";
 import Penalty from "@/app/admin/penalty/penalty";
-import ReportTester from "@/app/admin/penalty/report-tester";
 
 const Page = async () => {
   const penalties = await prisma.penalty.findMany({
@@ -39,7 +38,6 @@ const Page = async () => {
   }
   return (
     <div className="min-h-screen p-5">
-      <ReportTester />
       <div className="grid grid-cols-8 gap-2 w-full font-bold border-b-primary border-b-2 text-left">
         <div>Code</div>
         <div>Points</div>
